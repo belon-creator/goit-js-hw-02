8414780a526d0654be8df8d0f6c80ce91f480dc0
belon-creator <belon8911@gmail.com>
2020-04-27 13:58:45 +0300

    fix 6 task

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,28 @@
 const numbers = [];
 
 while (true) {
-  let input = +prompt('Введите число:');
-  numbers.push(input);
-
-  if (input === '' || input === null) {
-    let total = 0;
-    for (let i = 0; i < numbers.length; i++) {
-      total += numbers[i];
-    }
-    alert(`Общая сумма чисел равна ${total}`);
+  let input = prompt('Введите число');
+  if (input === null) {
+    console.log('Отменено пользователем!');
     break;
   }
+
+  input = Number(input);
+  const notANumber = Number.isNaN(input);
+  if (notANumber) {
+    alert('Было введено не число, попробуйте еще раз');
+    continue;
+  }
+  numbers.push(input);
 }
+
+function arraySum(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
+console.log(numbers);
+const total = arraySum(numbers);
+console.log(total);
